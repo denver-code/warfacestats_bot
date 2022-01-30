066606db2265734351c8aa35822c744eb6249c6f
Igor Savenko <csigorek@gmail.com>
2022-01-30 23:21:02 +0200

    update start text

--- internal/start.py
+++ internal/start.py
@@ -5,11 +5,14 @@ async def start_event(message: types.Message):
 <b>Hello at WarfaceStatsBot - International!</b>
 Here you can see statistics about users and clans on Russian or international servers!
 
+<b>Bot is Under Development! If you found bug, write to developer @idenver_bot</b>
+
 <b>To get information about a user, use:</b>
 /player [DenSec]
 Replace "[DenSec]" with a nickname.
 To specify a Russian server, add "ru" to the command:
 /player [DenSec] ru
+Or basic send text message with nickname and server.
 
 <b>To find out online servers:</b>
 /online - will show the number of users on the European server
