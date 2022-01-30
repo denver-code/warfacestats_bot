0672d0fc454f46937095aae25d3f4d4a43b521d3
Igor Savenko <csigorek@gmail.com>
2022-01-30 04:17:08 +0200

    register all commands

--- handlers/register_internal_command.py
+++ handlers/register_internal_command.py
@@ -1,5 +1,10 @@
 from aiogram import types, Dispatcher
 from internal.credits import credits_event
+from internal.player_stats import player_stats
+from internal.online_stats import region_online
+
 
 def register_internal_commands(dp: Dispatcher):
     dp.register_message_handler(credits_event, commands="credits")
+    dp.register_message_handler(player_stats, commands="player")
+    dp.register_message_handler(region_online, commands="online")
