81ab2fc5d5c58cc0f4a7f369661d71091a41c2ce
Igor Savenko <csigorek@gmail.com>
2022-01-30 23:05:27 +0200

    added start command

---
+++ internal/start.py
@@ -0,0 +1,20 @@
+from aiogram import types
+
+async def start_event(message: types.Message):
+    message_to_send = """
+<b>Hello at WarfaceStatsBot - International!</b>
+Here you can see statistics about users and clans on Russian or international servers!
+
+<b>To get information about a user, use:</b>
+/player [DenSec]
+Replace "[DenSec]" with a nickname.
+To specify a Russian server, add "ru" to the command:
+/player [DenSec] ru
+
+<b>To find out online servers:</b>
+/online - will show the number of users on the European server
+/online ru - on the Russian server.
+
+/credits - find out those. bot information.
+"""
+    await message.answer(message_to_send, parse_mode="HTML")
