47655d494ae09eb142869a374e353761fa42e464
Igor Savenko <csigorek@gmail.com>
2022-01-30 04:16:47 +0200

    updated credits command

--- internal/credits.py
+++ internal/credits.py
@@ -9,7 +9,9 @@ async def credits_event(message: types.Message):
     await message.answer(f"""
 *WFStats - Internal:*
 
-*Developer:* [Igor Savenko aka Denver](tg://user?id={os.getenv("OWNER_ID")})
+*Developer:*
+- *Telegram* - [Igor Savenko aka Denver](tg://user?id={os.getenv("OWNER_ID")})
+- *Warface* - [DenSec](https://pc.warface.com/custom/ub/MV8xNTUwNTAxOHNvbWVfdG9rZW5fa2V5/b817074b473260eba113eabbd1967fbb/bar.jpg)
 
 Stack:
 - *Python3* + *AIOgram*
@@ -17,4 +19,4 @@ Stack:
 *Used APIs:*
 [WFStats.cf api](https://api.wfstats.cf/)
 [WFStats.cf website](https://wfstats.cf)
-""", parse_mode="Markdown", disable_web_page_preview=True)
+""", parse_mode="Markdown", disable_web_page_preview=False)
